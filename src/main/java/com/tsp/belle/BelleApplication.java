30c501931224abf84623f1cf8af41225faf2a421
dhsb-4 <3042154624@qq.com>
2020-03-18 15:12:25 +0800

    Initial commit

---
+++ src/main/java/com/tsp/belle/BelleApplication.java
@@ -0,0 +1,13 @@
+package com.tsp.belle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tsp/belle/BelleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsp.belle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
