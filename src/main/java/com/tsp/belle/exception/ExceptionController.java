bb4925a4628d4064360e971d494e1a96218afb99
likewindz <1366845556@qq.com>
2020-03-19 11:02:02 +0800

    exception controller

---
+++ src/main/java/com/tsp/belle/exception/ExceptionController.java
@@ -0,0 +1,19 @@
+package com.tsp.belle.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * @author likeWind
+ * @date created in 11:00 2020/3/19
+ * @description
+ */
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(Exception.class)
+    public String toError(){
+        return "404";
+    }
+
+}
