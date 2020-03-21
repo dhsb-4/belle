029c0d794b60a97df469c97e2a56ed58b7887ac0
likewindz <1366845556@qq.com>
2020-03-21 11:02:29 +0800

    文件上传封装

--- src/test/java/com/tsp/belle/BelleApplicationTests.java
+++ src/test/java/com/tsp/belle/BelleApplicationTests.java
@@ -1,16 +1,35 @@
 package com.tsp.belle;
 
+import com.tsp.belle.file.FileService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
 class BelleApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-    @Test
-    public void test01(){
 
+    @Autowired
+    private FileService fileService;
+
+    @Test
+    void upload() throws FileNotFoundException {
+        File file = new File("C:\\Users\\Administrator\\Desktop\\dio.jpg");
+        InputStream stream = new FileInputStream(file);
+        String path = fileService.upload(stream, "jpg");
+        System.out.println(path);
     }
+
+    @Test
+    void download(){
+        String path = "M00/00/00/rB8Ht151gtOAc-twAAEcrQx4y0E881.jpg";
+        fileService.download(path);
+    }
+
+
 }
